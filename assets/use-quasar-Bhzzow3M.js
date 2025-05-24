88b30ea33ce0922cf4bc85ac2a1e4e274cc494f4
CyberStorebcb <cyberstorebcb@gmail.com>
2025-05-24 09:23:00 -0300

    Updates

--- assets/use-quasar-Bhzzow3M.js
+++
@@ -1,1 +0,0 @@
-import{e as a,av as r}from"./index-Gk3z1Mj9.js";function s(){return a(r)}export{s as u};
---
+++ assets/use-quasar-IUskAzoi.js
@@ -0,0 +1,1 @@
+import{e as a,av as r}from"./index-DXMZALbm.js";function s(){return a(r)}export{s as u};
